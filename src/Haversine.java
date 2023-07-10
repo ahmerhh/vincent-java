bfc39810544ce9c7aafe8489bb24cfe2dbd24f7c
Ahmer <ahmerhabibb@gmail.com>
2023-07-10 12:53:39 +0100

    Changing to Vincenty Formula

--- src/Haversine.java
+++ src/Haversine.java
@@ -1,31 +1,63 @@
 import java.util.Scanner;
 
+//Java program to calculate distance
 public class Haversine {
  
-    private static final double EARTH_RADIUS = 6371; // Radius of the Earth in kilometers
+    private static final double EARTH_RADIUS = 6371.0; // Radius of the Earth in kilometers
  
-    public static double calculateHaversineDistance(double lat1, double lon1, double lat2, double lon2) {
+    public static double calculateVincentyDistance(double lat1, double lon1, double lat2, double lon2) {
         // Convert latitude and longitude to radians
         double lat1Rad = Math.toRadians(lat1);
         double lon1Rad = Math.toRadians(lon1);
         double lat2Rad = Math.toRadians(lat2);
         double lon2Rad = Math.toRadians(lon2);
 
-        // Calculate the differences in latitude and longitude
-        double deltaLat = lat2Rad - lat1Rad;
+        // Difference in longitudes
         double deltaLon = lon2Rad - lon1Rad;
 
-        // Apply Haversine formula
-        double a = Math.pow(Math.sin(deltaLat / 2), 2) +
-                   Math.cos(lat1Rad) * Math.cos(lat2Rad) *
-                   Math.pow(Math.sin(deltaLon / 2), 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        // Vincenty formula variables
+        double a = EARTH_RADIUS;
+        double b = EARTH_RADIUS;
+        double f = 1 / 298.257223563; // Earth's flattening
+        double p1 = Math.atan((1 - f) * Math.tan(lat1Rad));
+        double p2 = Math.atan((1 - f) * Math.tan(lat2Rad));
+        double l = deltaLon;
 
-        // Calculate the distance
-        double distance = EARTH_RADIUS * c;
+        double sinLambda, cosLambda, sinSigma, cosSigma, sigma, sinAlpha, cosSqAlpha, cos2SigmaM;
+        double lambda = l;
+        double lambdaPrev = Double.MAX_VALUE;
+
+        //initializing formula
+        while (Math.abs(lambda - lambdaPrev) > 1e-12) {
+            sinLambda = Math.sin(lambda);
+            cosLambda = Math.cos(lambda);
+            sinSigma = Math.sqrt(Math.pow((Math.cos(p2) * sinLambda), 2) +
+                                 Math.pow((Math.cos(p1) * Math.sin(p2) - Math.sin(p1) * Math.cos(p2) * cosLambda), 2));
+            cosSigma = Math.sin(p1) * Math.sin(p2) + Math.cos(p1) * Math.cos(p2) * cosLambda;
+            sigma = Math.atan2(sinSigma, cosSigma);
+            sinAlpha = Math.cos(p1) * Math.cos(p2) * sinLambda / sinSigma;
+            cosSqAlpha = 1 - Math.pow(sinAlpha, 2);
+            cos2SigmaM = cosSigma - 2 * Math.sin(p1) * Math.sin(p2) / cosSqAlpha;
+
+            double C = f / 16 * cosSqAlpha * (4 + f * (4 - 3 * cosSqAlpha));
+            lambdaPrev = lambda;
+            lambda = l + (1 - C) * f * sinAlpha *
+                     (sigma + C * sinSigma * (cos2SigmaM + C * cosSigma * (-1 + 2 * Math.pow(cos2SigmaM, 2))));
+        }
+
+        //Calculating distance using Vincenty Formula
+        double uSq = cosSqAlpha * ((Math.pow(a, 2) - Math.pow(b, 2)) / Math.pow(b, 2));
+        double A = 1 + uSq / 16384 * (4096 + uSq * (-768 + uSq * (320 - 175 * uSq)));
+        double B = uSq / 1024 * (256 + uSq * (-128 + uSq * (74 - 47 * uSq)));
+        double deltaSigma = B * sinSigma * (cos2SigmaM + B / 4 *
+                            (cosSigma * (-1 + 2 * Math.pow(cos2SigmaM, 2)) - B / 6 * cos2SigmaM *
+                            (-3 + 4 * Math.pow(sinSigma, 2)) * (-3 + 4 * Math.pow(cos2SigmaM, 2))));
+
+        double distance = b * A * (sigma - deltaSigma);
         return distance;
     }
  
+    //Driver code
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -43,7 +75,7 @@ public class Haversine {
 
         scanner.close();
         
-        double distance = calculateHaversineDistance(lat1, lon1, lat2, lon2);
+        double distance = calculateVincentyDistance(lat1, lon1, lat2, lon2);
         System.out.println("The distance between the two points is: " + distance + " kilometers.");
     }
 }
